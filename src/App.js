6736c78502de0eedb8a9e019bf32e27b7f9ff6d2
Creatis <hello@tkrl.in>
2025-08-12 11:36:06 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NaturalDyeJournal from './components/NaturalDyeJournal';
+
+function App() {
+  return (
+    <div className="App">
+      <NaturalDyeJournal />
+    </div>
+  );
+}
+
+export default App;
